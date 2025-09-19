08ea4f97e29908c9d0f6b9d4bb8076cba8bb314f
godavdev <godavdeveloper@gmail.com>
2025-09-18 19:59:41 -0600

    Add lucide-react dependency and implement post liking feature
    - Added lucide-react to dependencies in package.json and package-lock.json.
    - Enhanced Post component to include author information and a like button with functionality to toggle likes.
    - Implemented search functionality in App component to filter posts by text.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,64 @@
-import Post from "./components/Post"
+import { useState } from "react"
+import Post, { type PostProps } from "./components/Post"
 
-const arr = [123, 33, 30, 20, 10, 40]
+const posts: PostProps[] = [
+  {
+    id: "1",
+    author: "David",
+    liked: false,
+    text: "Hola como estas",
+    likesNum: 12,
+  },
+  {
+    id: "a",
+    author: "Alejandro",
+    liked: true,
+    text: "Adios",
+    likesNum: 4,
+  },
+  {
+    id: "b",
+    author: "Oscar",
+    liked: false,
+    text: "Post numero 3",
+    likesNum: 2,
+  },
+]
 
 export default function App() {
+  const [search, setSearch] = useState("")
+  const handleClear = () => {
+    setSearch("")
+  }
+
+  const filteredPosts = posts.filter((post) =>
+    post.text.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
     <div className="w-full h-screen bg-amber-400 flex flex-col gap-2 items-center p-4">
-      {arr.map((value, index) => (
+      <div className="p-2 flex flex-col gap-2 items-center justify-center w-xl">
+        <p className="font-bold text-xl">Busqueda</p>
+        <input
+          className="bg-white w-full text-xl border rounded-md border-gray-400 px-3 py-1.5"
+          placeholder="Busqueda por texto..."
+          type="text"
+          value={search}
+          onChange={({ target: { value } }) => {
+            setSearch(value)
+          }}
+        />
+        <button
+          className="bg-white p-2 rounded border border-gray-400 hover:scale-105 transition-all cursor-pointer"
+          onClick={handleClear}
+        >
+          Limpiar busqueda
+        </button>
+      </div>
+      {filteredPosts.map((value) => (
         <Post
-          key={index}
-          text={`Este es el texto ${index}, suma del valor con index: ${
-            index + value
-          }`}
+          key={value.id}
+          {...value}
         />
       ))}
     </div>
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,7 +1,42 @@
-const Post = ({ text }: { text: string }) => {
+import { Heart } from "lucide-react"
+import { useState } from "react"
+
+export interface PostProps {
+  id: string
+  author: string
+  text: string
+  liked: boolean
+  likesNum: number
+}
+
+const Post = ({ text, author, liked, likesNum }: PostProps) => {
+  const [currentLiked, setCurrentLiked] = useState(liked)
+  const [currentLikesNum, setCurrentLikesNum] = useState(likesNum)
+
+  const toggleLike = () => {
+    if (currentLiked) {
+      setCurrentLikesNum((prev) => prev - 1)
+    } else {
+      setCurrentLikesNum((prev) => prev + 1)
+    }
+    setCurrentLiked((prev) => !prev)
+  }
+
   return (
-    <div className="w-xl bg-white border border-gray-400 rounded h-40">
-      {text}
+    <div className="w-xl bg-white border border-gray-400 rounded-lg  p-4 flex flex-col gap-2">
+      <div className="text-lg font-medium">{text}</div>
+      <div className="flex w-full justify-end text-sm text-gray-500">
+        Posteado por: {author}
+      </div>
+      <div className="flex gap-1">
+        <Heart
+          onClick={toggleLike}
+          className={`size-6 ${
+            currentLiked ? "text-pink-400" : "text-gray-500"
+          }`}
+        />
+        <div className="font-medium">{currentLikesNum}</div>
+      </div>
     </div>
   )
 }
